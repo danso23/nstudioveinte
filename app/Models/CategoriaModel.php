a881a486b3a048106beed844bce4dd360629b6b7
Iruhary <iruhary@gmail.com>
2021-10-19 22:35:19 -0500

    Se agregan detalles de diseño

--- app/Models/CategoriaModel.php
+++ app/Models/CategoriaModel.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class CategoriaModel extends Model{
     protected $table = 'categorias';
     protected $primaryKey = 'id_categoria';
-    protected $fillable = ['nombre_categoria', 'desc_categoria', 'activo'];
+    protected $fillable = ['nombre_categoria', 'icono', 'desc_categoria', 'activo'];
 }
