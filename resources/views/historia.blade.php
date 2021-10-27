62fea758896aec3990e11a86be568edcf80202d4
Iruhary <iruhary@gmail.com>
2021-10-26 22:31:18 -0500

    Se añade pantalla de la historia

--- resources/views/historia.blade.php
+++ resources/views/historia.blade.php
@@ -22,6 +22,23 @@
             </div>
         </div>
     </section>
+    <section>
+        <div class="container">
+            <div class="row align-items-start text-center" style="justify-content:center;">
+                <div class="col-lg-4 col-s-12">
+                    <p class="txt-cafe-cursivas" style="text-align: right;">Visión</p>
+                    <p class="textos-small" style="text-align: right;">Nuestra visión es ofrecerle a las mujeres productos de calidad, con precios accesibles y en tendencia. Procurando siempre la comodidad y estilos en cada una de nuestras prendas y accesorios con los mejores diseños representativos de la marca.</p>
+                </div>
+                <div class="col-lg-4 col-s-12 text-center" id="img-vision">
+                    <img src="{{ asset('public/img/misionvision.png') }}" alt="index.php" style="width:65%;" >
+                </div>
+                <div class="col-lg-4">
+                    <p class="txt-cafe-cursivas" style="text-align: left;">Misión</p>
+                    <p class="textos-small" style="text-align: left;">Posicionarnos en el mercado Yucateco como una marca líder a nivel local y nacional con el fin de ofrecer prendas a la vanguardia con la mejor calidad y a los mejores precios.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
 @section('script')
 <script>
