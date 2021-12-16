d6a7862f47cbd348318dbefa3c11690a96bbc8e0
Iruhary <iruhary@gmail.com>
2021-12-15 20:45:02 -0600

    Cambios registro e inicio

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
                             <label for="name" class="col-md-4 col-form-label textos-pagos">{{ __('Nombre completo') }}</label>
 
                             <div class="col-md-12">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control textos-small @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -29,9 +29,22 @@
                             <label for="email" class="col-md-4 col-form-label textos-pagos">{{ __('Correo electrónico') }}</label>
 
                             <div class="col-md-12">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control textos-small @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="telefono" class="col-md-4 col-form-label textos-pagos">{{ __('Teléfono') }}</label>
+
+                            <div class="col-md-12">
+                                <input id="telefono" type="number" maxlength="10" minlength="10" class="form-control textos-small @error('telefono') is-invalid @enderror" name="telefono" value="{{ old('telefono') }}" required autocomplete="telefono">
+
+                                @error('telefono')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -43,7 +56,7 @@
                             <label for="password" class="col-md-4 col-form-label textos-pagos">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-12">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control textos-small @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -57,13 +70,13 @@
                             <label for="password-confirm" class="col-md-4 col-form-label textos-pagos">{{ __('Confirma tu contraseña') }}</label>
 
                             <div class="col-md-12">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="textos-small form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-pink">
+                                <button type="submit" class="btn btn-cafe">
                                     {{ __('Registrarme') }}
                                 </button>
                             </div>
